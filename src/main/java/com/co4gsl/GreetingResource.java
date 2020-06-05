d8a6305d243bdf795afc8c989996652a08a596a2
RPAYAL <rpayal@hotmail.com>
2020-06-05 18:51:43 +0100

    Getting greeting from properties

--- src/main/java/com/co4gsl/GreetingResource.java
+++ src/main/java/com/co4gsl/GreetingResource.java
@@ -1,16 +1,22 @@
 package com.co4gsl;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Optional;
 
 @Path("/greeting")
 public class GreetingResource {
 
+    @ConfigProperty(name = "greeting")
+    public Optional<String> greeting;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return greeting.orElse("Namaste!!");
     }
 }
